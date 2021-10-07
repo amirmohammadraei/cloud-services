4922ae7aba51ad818506af3634595fe8d8122709
Amirmohammad Raei <amir.m.raei@gmail.com>
2021-10-08 02:18:31 +0330

    add story model

--- cloud_services/core/models.py
+++ cloud_services/core/models.py
@@ -7,4 +7,8 @@ class Comment(models.Model):
     photo = models.IntegerField(null=False, blank=False)
     is_toxic = models.BooleanField(default=False)
 
-    
+
+class Story(models.Model):
+    username = models.CharField(max_length=20, null=False, blank=False)
+    comment = models.CharField(max_length=1000, null=False, blank=False)
+    photo = models.IntegerField(null=False, blank=False)
