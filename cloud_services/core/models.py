93c65b19af3b5d81df5d9aa097892c5926c89c30
Amirmohammad Raei <amir.m.raei@gmail.com>
2021-10-08 20:53:28 +0330

    add str for model

--- cloud_services/core/models.py
+++ cloud_services/core/models.py
@@ -8,9 +8,15 @@ class Comment(models.Model):
     is_toxic = models.BooleanField(default=False)
     music = models.FileField(upload_to='media', null=True)
 
+    def __str__(self):
+        return f'{self.username}'
+
 
 class Story(models.Model):
     username = models.CharField(max_length=20, null=False, blank=False)
     story = models.TextField(null=False, blank=False)
     photo = models.IntegerField(null=False, blank=False)
     music = models.FileField(upload_to='music', null=True)
+
+    def __str__(self):
+        return f'{self.username}'
